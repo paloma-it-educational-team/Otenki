e13906844ee3297068e10d5b54375bf5f306bd93
Shingo Morimoto <shingo.morimoto@paloma.co.jp>
2019-07-04 00:06:51 +0900

    新規作成

--- Otenki/Form1.Designer.cs
+++ Otenki/Form1.Designer.cs
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 67);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(228, 252);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "明日の天気";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Otenki/Form1.cs
+++ Otenki/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestSharp;
 
 namespace Otenki
 {
@@ -16,5 +17,113 @@ namespace Otenki
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient();
+            var request = new RestRequest();
+            client.BaseUrl = new Uri("http://weather.livedoor.com/forecast/webservice/json/v1");
+
+            request.Method = Method.GET;
+            request.AddParameter("city", "230010", ParameterType.GetOrPost);
+            var response = client.Execute(request);
+            var tenki = Newtonsoft.Json.JsonConvert.DeserializeObject<Tenki>(response.Content);
+            pictureBox1.ImageLocation = tenki.forecasts[1].image.url;
+            label1.Text =
+                tenki.forecasts[1].dateLabel + "の天気は" + tenki.forecasts[1].telop + "\r\n" +
+                "最高気温：" + tenki.forecasts[1].temperature.max.celsius + "℃\r\n" +
+                "最低気温：" + tenki.forecasts[1].temperature.min.celsius + "℃";
+        }
+
+        public class Tenki
+        {
+            public Pinpointlocation[] pinpointLocations { get; set; }
+            public string link { get; set; }
+            public Forecast[] forecasts { get; set; }
+            public Location location { get; set; }
+            public DateTime publicTime { get; set; }
+            public Copyright copyright { get; set; }
+            public string title { get; set; }
+            public Description description { get; set; }
+        }
+
+        public class Location
+        {
+            public string city { get; set; }
+            public string area { get; set; }
+            public string prefecture { get; set; }
+        }
+
+        public class Copyright
+        {
+            public Provider[] provider { get; set; }
+            public string link { get; set; }
+            public string title { get; set; }
+            public Image image { get; set; }
+        }
+
+        public class Image
+        {
+            public int width { get; set; }
+            public string link { get; set; }
+            public string url { get; set; }
+            public string title { get; set; }
+            public int height { get; set; }
+        }
+
+        public class Provider
+        {
+            public string link { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Description
+        {
+            public string text { get; set; }
+            public DateTime publicTime { get; set; }
+        }
+
+        public class Pinpointlocation
+        {
+            public string link { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Forecast
+        {
+            public string dateLabel { get; set; }
+            public string telop { get; set; }
+            public string date { get; set; }
+            public Temperature temperature { get; set; }
+            public Image1 image { get; set; }
+        }
+
+        public class Temperature
+        {
+            public Min min { get; set; }
+            public Max max { get; set; }
+        }
+
+        public class Min
+        {
+            public string celsius { get; set; }
+            public string fahrenheit { get; set; }
+        }
+
+        public class Max
+        {
+            public string celsius { get; set; }
+            public string fahrenheit { get; set; }
+        }
+
+        public class Image1
+        {
+            public int width { get; set; }
+            public string url { get; set; }
+            public string title { get; set; }
+            public int height { get; set; }
+        }
+
+
     }
 }
